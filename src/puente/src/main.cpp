d150d987b47a851307ce122cfe7ebcdb29d6a2a9
alihyderr <ajah1@alu.ua.es>
2020-03-07 13:12:28 +0100

    store twist

--- src/puente/src/main.cpp
+++ src/puente/src/main.cpp
@@ -10,13 +10,27 @@
 #include <message_filters/time_synchronizer.h>
 #include <message_filters/sync_policies/approximate_time.h>
 
+#include <stdio.h>
+
 using namespace std;
 using namespace sensor_msgs;
 using namespace message_filters;
 
-void callback(const sensor_msgs::ImageConstPtr& msg, const geometry_msgs::TwistStampedConstPtr &image)
+unsigned int contadorDatos = 0;
+
+void callback(const sensor_msgs::ImageConstPtr& msg, const geometry_msgs::TwistStampedConstPtr& vel)
 {
-  std::cout << msg->header.stamp << ", " << image->header.stamp << "\n";
+  // save velv data to file with format: counter x y z
+  FILE* imagenDataFile;
+  imagenDataFile = fopen( "imagen_velocidad.data", "a" );
+  fprintf( imagenDataFile,
+    "%u %f %f %f\n",
+    contadorDatos, vel->twist.linear.x, vel->twist.linear.y, vel->twist.angular.z
+    );
+  fclose( imagenDataFile );
+  // save image
+  
+  ++contadorDatos;
 }
 
 int main(int argc, char **argv)
@@ -31,19 +45,12 @@ int main(int argc, char **argv)
   typedef message_filters::Synchronizer<MySyncPolicy> Sync;
   boost::shared_ptr<Sync> sync_;
 
-/*
-  message_filters::Subscriber<sensor_msgs::Image> image_sub(n1, "robot1/camera/rgb/image_raw", 1);
-  message_filters::Subscriber<geometry_msgs::TwistStamped> info_sub(n2, "chatter", 1);
-  TimeSynchronizer<Image, geometry_msgs::TwistStamped> sync(image_sub, info_sub, 10);
-  sync.registerCallback(boost::bind(&callback, _1, _2));
-*/
+  sub_1_.subscribe(nh, "robot1/camera/rgb/image_raw", 1);
+  sub_2_.subscribe(nh, "chatter", 1);
+  sync_.reset(new Sync(MySyncPolicy(10), sub_1_, sub_2_));
+  sync_->registerCallback(boost::bind(callback, _1, _2));
 
-    sub_1_.subscribe(nh, "robot1/camera/rgb/image_raw", 1);
-    sub_2_.subscribe(nh, "chatter", 1);
-    sync_.reset(new Sync(MySyncPolicy(10), sub_1_, sub_2_));
-    sync_->registerCallback(boost::bind(callback, _1, _2));
-
-  ros::spin();
+ros::spin();
 
   return 0;
 }
