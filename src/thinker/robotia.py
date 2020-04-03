dd6e4b6cedfb4051df3625888af4a4711b60d667
Jorge Amil Perez Ponsoda <japp7@alu.ua.es>
2020-04-03 17:13:48 +0200

    mobilenet

--- src/thinker/robotia.py
+++ src/thinker/robotia.py
@@ -1,58 +1,80 @@
 #!/usr/bin/env python
 
-# link de referencia
+# links de referencia
 # http://projectsfromtech.blogspot.com/2017/10/visual-object-recognition-in-ros-using.html
 # https://github.com/kungfrank/ros_inception_v3/blob/master/classify_image.py
-
 # https://www.pyimagesearch.com/2019/01/21/regression-with-keras/
 
+from __future__ import print_function
+import roslib
 import rospy
-from sensor_msgs.msg import Image
-from std_msgs.msg import String
-from cv_bridge import CvBridge
 import cv2
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+from geometry_msgs.msg import Twist
 import numpy as np
-import tensorflow as tf
-#from tensorflow.models.image.imagenet import classify_image
+from tensorflow.python.keras.applications.mobilenet import MobileNet
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense, Flatten
+from tensorflow.python.keras import backend as K
 
-class RosTensorFlow():
+K.set_image_data_format('channels_last')
+
+def obtenerModelo():
+  modelo_preentrenado = MobileNet(input_shape=(224, 224, 3), include_top=False, weights=None)
+  modelo_modificado = Sequential([modelo_preentrenado, Flatten(), Dense(256, activation='relu'), Dense(3, activation='softmax')])
+  return modelo_modificado
+
+modelo = obtenerModelo()
+
+class RosTensorFlow:
   def __init__(self):
-    print(tf.__version__)
-#    classify_image.maybe_download_and_extract()
-#    self._session = tf.Session()
-#    classify_image.create_graph()
-#    self._cv_bridge = CvBridge()
-#
-#    self._sub = rospy.Subscriber('image', Image, self.callback, queue_size=1)
-#    self._pub = rospy.Publisher('result', String, queue_size=1)
-#    self.score_threshold = rospy.get_param('~score_threshold', 0.1)
-#    self.use_top_k = rospy.get_param('~use_top_k', 5)
+    self.bridge = CvBridge()
+    self.image_sub = rospy.Subscriber("/robot1/camera/rgb/image_raw",Image, self.imageCallback)
+    self.cmd_vel = rospy.Publisher('/robot1/mobile_base/commands/velocity', Twist, queue_size=1)
+    self.lastImage = None
+    self.process = False
 
-  def callback(self, image_msg):
-    print(tf.__version__)
-#    cv_image = self._cv_bridge.imgmsg_to_cv2(image_msg, "bgr8")
-#    # copy from
-#    # https://github.com/tensorflow/tensorflow/blob/master/tensorflow/models/image/imagenet/classify_image.py
-#    image_data = cv2.imencode('.jpg', cv_image)[1].tostring()
-#    # Creates graph from saved GraphDef.
-#    softmax_tensor = self._session.graph.get_tensor_by_name('softmax:0')
-#    predictions = self._session.run(
-#        softmax_tensor, {'DecodeJpeg/contents:0': image_data})
-#    predictions = np.squeeze(predictions)
-#    # Creates node ID --> English string lookup.
-#    node_lookup = classify_image.NodeLookup()
-#    top_k = predictions.argsort()[-self.use_top_k:][::-1]
-#    for node_id in top_k:
-#      human_string = node_lookup.id_to_string(node_id)
-#      score = predictions[node_id]
-#      if score > self.score_threshold:
-#        rospy.loginfo('%s (score = %.5f)' % (human_string, score))
-#        self._pub.publish(human_string)
+  def imageCallback(self,data):
+    try:
+      cv_image = cv2.resize(self.bridge.imgmsg_to_cv2(data, "rgb8"), (224, 224), interpolation = cv2.INTER_CUBIC)
+      self.lastImage = cv_image
+      self.process = True
+    except CvBridgeError as e:
+      print(e)
+  
+  def run(self):
+    move_cmd = Twist()
+    move_cmd.linear.x = 0
+    move_cmd.angular.z = 0
+    while True:
+      if self.process:
+        ImageList_ = []
+        ImageList_.append(np.asarray(self.lastImage).astype('float32')/255)
+        predicciones = modelo.predict(np.asarray(ImageList_).reshape(1, 224, 224, 3))
+        cv2.imshow("Image window", self.lastImage)
+        cv2.waitKey(3)
+        move_cmd.linear.x = move_cmd.linear.y = move_cmd.angular.z = 0
 
-  def main(self):
-    rospy.spin()
+        if np.argmax(predicciones) == 0:
+          move_cmd.angular.z += 0.5
+          move_cmd.linear.x += 0.3
+        elif np.argmax(predicciones) == 2:
+          move_cmd.angular.z += -0.5
+          move_cmd.linear.x += 0.3
+        else:
+          move_cmd.angular.z += 0.0
+          move_cmd.linear.x += 0.5
+
+      self.cmd_vel.publish(move_cmd)
+    cv2.destroyAllWindows()
+
+def inicio():
+  modelo.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
+  modelo.load_weights('./ros.hdf5')
+  navegacion = RosTensorFlow()
+  rospy.init_node('rostensorflow', anonymous=True)
+  navegacion.run()
 
 if __name__ == '__main__':
-  rospy.init_node('rostensorflow')
-  tensor = RosTensorFlow()
-  tensor.main()
+	inicio()
