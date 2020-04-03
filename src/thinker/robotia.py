d4943bdb16e2e5c98a27cfbb2aa03f49820eba46
Jorge Amil Perez Ponsoda <japp7@alu.ua.es>
2020-04-03 17:50:38 +0200

    se mueve

--- src/thinker/robotia.py
+++ src/thinker/robotia.py
@@ -7,6 +7,7 @@
 
 from __future__ import print_function
 import roslib
+roslib.load_manifest('thinker')
 import rospy
 import cv2
 from sensor_msgs.msg import Image
@@ -14,18 +15,27 @@ from cv_bridge import CvBridge, CvBridgeError
 from geometry_msgs.msg import Twist
 import numpy as np
 from tensorflow.python.keras.applications.mobilenet import MobileNet
-from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.layers import Dense, Flatten
+from tensorflow.python.keras.models import Model
+from tensorflow.python.keras.layers import Dense, Input
 from tensorflow.python.keras import backend as K
 
+tam_entrada = (224, 224, 3)
+tensor_entrada = Input(shape=tam_entrada)
 K.set_image_data_format('channels_last')
 
-def obtenerModelo():
-  modelo_preentrenado = MobileNet(input_shape=(224, 224, 3), include_top=False, weights=None)
-  modelo_modificado = Sequential([modelo_preentrenado, Flatten(), Dense(256, activation='relu'), Dense(3, activation='softmax')])
-  return modelo_modificado
+def obtenerModelo(tam_entrada):
+    modelo_preentrenado = MobileNet(input_shape=tam_entrada,
+                                input_tensor=tensor_entrada,
+                                pooling='avg',
+                                include_top=False,
+                                weights=None)
+    salida = modelo_preentrenado.output
+    prediccion = Dense(1, activation="sigmoid")(salida)
+    
+    modelo_modificado = Model(inputs=tensor_entrada, outputs=prediccion)
+    return modelo_modificado
 
-modelo = obtenerModelo()
+modelo = obtenerModelo(tam_entrada)
 
 class RosTensorFlow:
   def __init__(self):
@@ -37,12 +47,12 @@ class RosTensorFlow:
 
   def imageCallback(self,data):
     try:
-      cv_image = cv2.resize(self.bridge.imgmsg_to_cv2(data, "rgb8"), (224, 224), interpolation = cv2.INTER_CUBIC)
-      self.lastImage = cv_image
+      imagen_opencv = cv2.resize(self.bridge.imgmsg_to_cv2(data, "rgb8"), (224, 224), interpolation = cv2.INTER_CUBIC)
+      self.lastImage = imagen_opencv
       self.process = True
-    except CvBridgeError as e:
-      print(e)
-  
+    except CvBridgeError as excepcion:
+      print(excepcion)
+
   def run(self):
     move_cmd = Twist()
     move_cmd.linear.x = 0
@@ -54,27 +64,24 @@ class RosTensorFlow:
         predicciones = modelo.predict(np.asarray(ImageList_).reshape(1, 224, 224, 3))
         cv2.imshow("Image window", self.lastImage)
         cv2.waitKey(3)
-        move_cmd.linear.x = move_cmd.linear.y = move_cmd.angular.z = 0
+        move_cmd.linear.x = move_cmd.linear.y = move_cmd.angular.z = 0;   
 
-        if np.argmax(predicciones) == 0:
+        if (predicciones[0][0]*100) < 50:
           move_cmd.angular.z += 0.5
           move_cmd.linear.x += 0.3
-        elif np.argmax(predicciones) == 2:
+        else:
           move_cmd.angular.z += -0.5
           move_cmd.linear.x += 0.3
-        else:
-          move_cmd.angular.z += 0.0
-          move_cmd.linear.x += 0.5
 
       self.cmd_vel.publish(move_cmd)
     cv2.destroyAllWindows()
 
-def inicio():
-  modelo.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
+def main():
+  modelo.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
   modelo.load_weights('./ros.hdf5')
   navegacion = RosTensorFlow()
-  rospy.init_node('rostensorflow', anonymous=True)
+  rospy.init_node('robotia', anonymous=True)
   navegacion.run()
 
 if __name__ == '__main__':
-	inicio()
+	main()
