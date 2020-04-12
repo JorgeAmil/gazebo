d5df6c5d5f4dc845f3ba89a553eb446081051958
Jorge Amil Perez Ponsoda <japp7@alu.ua.es>
2020-04-12 12:24:13 +0200

    se acabo

--- src/moduloStereo/src/moduloStereo.cpp
+++ src/moduloStereo/src/moduloStereo.cpp
@@ -20,20 +20,33 @@ void imageCallback(const ImageConstPtr& trasera1, const ImageConstPtr& trasera2)
   try {
     cout << "matrices" << endl;
     cv::Mat matriz1, salida1;
-    matriz1 = cv_bridge::toCvShare(trasera1, "mono8")->image;
+    matriz1 = cv_bridge::toCvShare(trasera1, "mono8")->image;//mono8
     cv::Mat matriz2, salida2;
-    matriz2 = cv_bridge::toCvShare(trasera2, "mono8")->image;
+    matriz2 = cv_bridge::toCvShare(trasera2, "mono8")->image;//bgr8
     //cout << "ajuste" << endl;
     //cv::cvtColor(matriz1, salida1, CV_8UC1);
     //cv::cvtColor(matriz2, salida2, CV_8UC1);
     cout << "disparidad" << endl;
-    cv::Mat disparity_out;
+    cv::Mat disparidad;
     cout << "compute" << endl;
-    Ptr< StereoBM > match = StereoBM::create();
-    match->compute(matriz1, matriz2, disparity_out);
-//    match->compute(salida1, salida2, disparity_out);
+//    Ptr<StereoBM> emparejador = StereoBM::create();
+    Ptr<StereoSGBM> emparejador = StereoSGBM::create(-55,32,11,120,2000,32,0,15,1000,16,StereoSGBM::MODE_HH);
+    emparejador->compute(matriz1, matriz2, disparidad);
+//    emparejador->compute(salida1, salida2, disparity_out);
+    cout << "normalizacion" << endl;
+    Mat disparidad_normalizada;
+    normalize(disparidad, disparidad_normalizada, 0, 255, CV_MINMAX, CV_8U);
+    cout << "reprojectar" << endl;
+    float matriz_transformacion[4][4] = {
+      {1,  0, 0, -matriz1.cols / 2.0},
+      {0, -1, 0, matriz1.cols / 2.0},
+      {0,  0, 0, -0.8 * matriz1.cols},
+      {0,  0, 1, 0}
+    };
+    Mat nube, conversion(4, 4, CV_32FC1, &matriz_transformacion);
+    reprojectImageTo3D(disparidad_normalizada, nube, conversion);
     cout << "show" << endl;
-    imshow("test",disparity_out * 16);
+    imshow("view",nube * 16);
     cout << "wait" << endl;
     cv::waitKey(30);
   }
